ef02090547bfb713802360d142f2bbc3347ad0d9
rtgenaapc <rtgena@student.apc.edu.ph>
2024-01-20 21:26:43 +0800

    Sign up, Login, Main Page

--- FINALS VERSION/ruth-and-jerry/src/app/app.routes.ts
+++ FINALS VERSION/ruth-and-jerry/src/app/app.routes.ts
@@ -5,4 +5,16 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'sign-up',
+    loadComponent: () => import('./sign-up/sign-up.page').then( m => m.SignUpPage)
+  },
+  {
+    path: 'main',
+    loadComponent: () => import('./main/main.page').then( m => m.MainPage)
+  },
 ];
