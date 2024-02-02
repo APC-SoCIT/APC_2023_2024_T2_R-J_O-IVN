edd6a1c56a8f7ce6232861ef55baba0b1953ddaa
rtgenaapc <rtgena@student.apc.edu.ph>
2024-02-02 09:20:45 +0800

    draft 1

--- FINALS - CUSTOMER SIDE/ruth-and-jerry-customer/src/app/app.routes.ts
+++ FINALS - CUSTOMER SIDE/ruth-and-jerry-customer/src/app/app.routes.ts
@@ -5,4 +5,20 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'main',
+    loadComponent: () => import('./main/main.page').then( m => m.MainPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'sign-up',
+    loadComponent: () => import('./sign-up/sign-up.page').then( m => m.SignUpPage)
+  },
+  {
+    path: 'forget-pass',
+    loadComponent: () => import('./forget-pass/forget-pass.page').then( m => m.ForgetPassPage)
+  },
 ];
